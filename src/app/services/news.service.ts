247e534b12b7972ada1887cd1714bc5f59f96f23
Damiano Maka <133500319+damiano-maka@users.noreply.github.com>
2024-02-15 13:43:01 +0100

    okk

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -9,7 +9,7 @@ import { Article, Response } from '../models/news.model';
 export class NewsService {
   http = inject(HttpClient);
   private newUrl = 'https://newsapi.org/v2'
-  private api ="306021cf39424031b1c3ac021ef1fb9b"
+  private api ="4070a172e2ee461f806f83700c34afb4"
 
   public getHeadLines(categoria : string,lingua: string , size: number):Observable<Response>{
    return this.http.get<Response>(`${this.newUrl}/everything?q=${categoria}&language=${lingua}&pageSize=${size}&apiKey=${this.api}`,)
